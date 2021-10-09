8593e5968a285c7f02b29bfaeb8ae9706cb34a86
Alessandra Moraes <alessandra.c.moraes@gmail.com>
2021-10-09 15:28:36 -0300

    Icluit arquivos do projeto

---
+++ js/main.js
@@ -0,0 +1,117 @@
+//var nome = "Alessandra Moraes";
+//var idade = "31";
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+//console.log (idade + idade2); 
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+//console.log(frase.toLocaleUpperCase());
+
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//alert(lista)
+//console.log (lista[1]);
+//console.log(lista.toString());
+//console.log(lista.join(" e "));
+
+/*
+var fruta = {nome: "maça", cor: "vermelha"};
+console.log(fruta);
+alert(fruta.cor);
+*/
+
+/*var fruta = [{nome: "maça", cor: "vermelha"}, {nome:"uva" , cor: "roxa"}];
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+
+/*
+var idade = prompt("Qual é sua idade?");
+if (idade>=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var count = 0;
+while(count <=5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*var count;
+for(count=0;count <=5; count++){
+    alert(count);
+}
+*/
+
+//var d = new Date();
+//alert (d);
+//alert(d.getDay());
+//alert(d.getHours());
+//alert(d.getMinutes());
+//alert(d.getMonth()+1);
+
+//function soma(n1, n2){
+//    return n1 + n2;
+//}
+
+//function setReplace(frase, nome, novo_nome){
+//    return frase.replace(nome, novo_nome)
+//}
+
+
+//alert(soma(5,10));
+//alert(setReplace("Vai Japão", "Japão", "Brasil"))
+
+/*function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+
+
+/*function clicou(){
+    //alert("Obrigado por clicar")  
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+
+function redirecionar(){
+    window.open("https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d?back=/track/html-web-developer");
+    //window.location.href = "https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d?back=/track/html-web-developer";
+}
+
+function trocar(elemento){
+    //alert ("trocar texto");
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse aqui";
+    elemento.innerHTML = "Obrigada por passar o mouse aqui";
+}
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+*/
+
